2ba4a31478478deb29f2a53d1067169e004959f4
robo-monk <filtap007@gmail.com>
2019-12-13 13:29:26 +0100

    upgrade with buttons and shit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var ui =0;
 var cpu=0;
+var pressing = false;
+var cpu_score = 0;
+var p_score = 0;
+var end = false;
 
 //1 rock | 2 paper | 3 scissors
 // 1-2 L | 1-3 W | 2-1 W | 2-3 L | 3-1 L | 3-2 W
@@ -34,7 +38,7 @@ function randrand_rps(){
 
     }
 
-    document.getElementById("cpu_out").innerText = "CPU: " + text;
+    document.getElementById("cpu").innerText = text;
   
     evaluate();
 }
@@ -58,6 +62,84 @@ function evaluate(){
         } else { out_msg = "WIN"; } //3-2
     }
 
-    document.getElementById("results").innerText = "RESULTS: " + out_msg;
+    assess(out_msg)
+    document.getElementById("results").innerText = "CPU: " + cpu_score + " YOU: " + p_score;
 
 }
+
+function assess(inp){
+
+    if (p_score < 5 && cpu_score < 5){
+
+        if (inp == "WIN") {
+            p_score += 1;
+        }
+
+        if (inp == "LOSS") {
+            cpu_score += 1
+        }
+
+        if (inp == "DRAW") {
+            cpu_score += 1
+            p_score += 1;
+        }
+
+    }else {
+
+        if (cpu_score<p_score){
+            document.getElementById("cpu").innerText = "YOU WON -- press any button to continue";
+        }else{
+            document.getElementById("cpu").innerText = "YOU LOST -- press any button to continue";
+        }
+
+        end = true;
+
+    }
+
+    return inp;
+
+}
+
+function buttonPress(e){
+
+    if (!end){
+        const button = document.querySelector(`button[data-key="${e.keyCode}"]`);
+        button.classList.add('animating');
+
+        if (!pressing) {
+            store_ui(e - 36);
+        }
+
+        pressing = true; //todo make pressing a list with currently pressed buttons
+    }else {
+        reset();
+    }
+    
+}
+
+function buttonUnpress(e) {
+
+    const button = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    button.classList.remove('animating');
+    pressing=false;
+
+}
+
+function reset(){
+
+    ui = 0;
+    cpu = 0;
+    pressing = false;
+    cpu_score = 0;
+    p_score = 0;
+    end = false;
+
+
+}
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+
+
+
+window.addEventListener('keydown', buttonPress);
+window.addEventListener('keyup', buttonUnpress)
